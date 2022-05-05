3ef695308b000fdf83e1b98c9bcae7710ff7e717
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-05 13:16:41 -0700

    Add files via upload

---
+++ Spawn Sprite.py
@@ -0,0 +1,36 @@
+"""
+LESSON: 5.4 - Sprites in Lists
+TECHNIQUE 3: Spawn Sprite
+DEMO 1
+"""
+import pygame
+import tsk
+pygame.init()
+
+window = pygame.display.set_mode([1018, 573])
+c = pygame.time.Clock()
+
+background = tsk.Sprite("OutdoorHills.jpg", 0, 0)
+beehive = tsk.Sprite("Beehive.png", 50, 100)
+bees = []
+
+drawing = True
+while drawing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drawing = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print("Bee!")
+            bee = tsk.Sprite("Bee.png", beehive.center_x, beehive.center_y)
+            bees.append(bee)
+
+    for bee in bees:
+        bee.center_x += 0.5 * c.get_time()
+
+    background.draw()
+    beehive.draw()
+    for bee in bees:
+        bee.draw()
+
+    pygame.display.flip()
+    c.tick(30)
