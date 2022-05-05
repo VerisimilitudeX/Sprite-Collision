ff7cbc83563faf55fd0dc727476f9e33d9500fcf
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-05 13:11:22 -0700

    Add files via upload

---
+++ Draw Scene.py
@@ -0,0 +1,34 @@
+"""
+LESSON: 5.4 - Sprites in Lists
+TECHNIQUE 1: Draw Scene
+DEMO 1
+"""
+
+import pygame
+import tsk
+pygame.init()
+
+window = pygame.display.set_mode([1018, 573])
+c = pygame.time.Clock()
+
+background = tsk.Sprite("Desert.jpg", 0, 0)
+crab = tsk.Sprite("RockCrab.png", 230, 250)
+rock1 = tsk.Sprite("BrownRock1.png", 100, 200)
+rock2 = tsk.Sprite("BrownRock2.png", 400, 350)
+rock3 = tsk.Sprite("BrownRock3.png", 700, 100)
+rock4 = tsk.Sprite("RoundGemPink.png", 700, 400)
+
+sprite_list = [background, rock1, rock2, rock3, rock4]
+
+drawing = True
+while drawing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drawing = False
+            
+    for sprite in sprite_list:
+        sprite.update(c.get_time())
+        sprite.draw
+        
+    pygame.display.flip()
+    c.tick(30)
