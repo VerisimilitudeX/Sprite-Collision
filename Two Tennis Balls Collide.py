f25e7bb1d46b71a0c0b903047cbfc23f23163c14
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-09 21:08:20 -0700

    Update Two Tennis Balls Collide.py

--- Two Tennis Balls Collide.py
+++ Two Tennis Balls Collide.py
@@ -1,8 +1,3 @@
-"""
-LESSON: 5.3 - Sprite Collision
-WARMUP 4
-"""
-
 import pygame
 import tsk
 import random
@@ -27,21 +22,17 @@ while drawing:
         if event.type == pygame.QUIT:
             drawing = False
 
-
-    # Check for collision
     if pygame.sprite.collide_rect(ball1, ball2):
         h_speed_1 = 0
         v_speed_1 = 0
         h_speed_2 = 0
         v_speed_2 = 0
 
-    # Move the balls
     ball1.center_x += h_speed_1 * c.get_time()
     ball1.center_y += v_speed_1 * c.get_time()
     ball2.center_x += h_speed_2 * c.get_time()
     ball2.center_y += v_speed_2 * c.get_time()
 
-    # Bounce on edges
     if ball1.center_x < 0 or ball1.center_x > 1018:
         h_speed_1 *= -1
     if ball1.center_y < 150 or ball1.center_y > 573:
@@ -52,7 +43,6 @@ while drawing:
     if ball2.center_y < 180 or ball2.center_y > 573:
         v_speed_2 *= -1
 
-    # Draw
     background.draw()
     ball1.draw()
     ball2.draw()
