bd5647a18d0989f59073f876ab58cbbe1490dff0
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-09 21:02:33 -0700

    Update Gem Color Change.py

--- Gem Color Change.py
+++ Gem Color Change.py
@@ -1,8 +1,3 @@
-"""
-LESSON: 5.3 - Sprite Collision
-TECHNIQUE 2: Onscreen Button
-PRACTICE 1
-"""
 import pygame
 import tsk
 pygame.init()
@@ -35,6 +30,3 @@ while drawing:
 
     pygame.display.flip()
     c.tick(30)
-
-
-# Turn in your Coding Exercise.
